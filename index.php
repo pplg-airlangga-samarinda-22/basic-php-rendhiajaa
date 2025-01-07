722c4ac2b6e69cb9a51c7172d67c6a8b82891c41
rendhiajaa <22_rendhisanjaya@student.smkti.net>
2025-01-07 09:07:04 +0800

    selesai

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-
+// simpan nama dan nisn klian ke dalam sebuah variabel
+    $nama = "Rendhi Sanjaya";
+    $nisn = "0084798357";
+// tampilkan atau cetak variabel ke dalam layar 
+    echo 'Nama : ' ,$nama ;
+    echo '<br>';
+    echo 'NISN : ' ,$nisn ;
 ?>
